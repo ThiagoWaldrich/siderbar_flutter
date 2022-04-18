141d946560b05debfb552148f258df73adb95366
ThiagoWaldrich <thiagowlf7@gmail.com>
2022-04-17 22:24:09 -0300

    Adjust in sidebar

--- lib/sidebar/sidebar.dart
+++ lib/sidebar/sidebar.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 class SideBar extends StatelessWidget {
   const SideBar({Key? key}) : super(key: key);
 
+  final bool side_bar_open = false;
   @override
   Widget build(BuildContext context) {
-    return Row(
+
+    final scree_width = MediaQuery.of(context).size.width;
+    return Positioned(
+      top: 0,
+      bottom: 0,
+      left: side_bar_open ? 0 : 0,
+      right: side_bar_open ? 0 : scree_width - 45, 
+        child: Row(
       children: <Widget>[
         Expanded(
           child: Container(
@@ -13,14 +21,14 @@ class SideBar extends StatelessWidget {
           ),
         ),
         Align(
-            alignment: Alignment(0, -0.9),
-            child: Container(
-              width: 35,
-              height: 110,
-              color: Color(0xFF262AAA),
-            ),
+          alignment: Alignment(0, -0.9),
+          child: Container(
+            width: 35,
+            height: 110,
+            color: Color(0xFF262AAA),
           ),
+        ),
       ],
-    );
+    ));
   }
 }
