dd4e796bf31a3d7e108f96068d95ccd1f495dfcf
ThiagoWaldrich <thiagowlf7@gmail.com>
2022-04-17 22:19:15 -0300

    Add a Center Text

--- lib/pages/orders.dart
+++ lib/pages/orders.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class Order extends StatelessWidget {
+  const Order({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Text(
-          "HomePage",
+          "Orders",
           style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
         ),
     );
